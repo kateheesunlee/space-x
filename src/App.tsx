ff0c9a1cebf7c9f142c62aa5692d5fabe9d5b171
Kate Lee <katelee@Kates-MacBook-Pro.local>
2022-11-15 19:05:45 -0800

    set up apollo grpahql

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useQuery, gql } from '@apollo/client';
 import './App.css';
 
 function App() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+  console.log({ data });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      hello
+    </>
   );
 }
+
+const GET_LAUNCHES = gql`
+  query GetPastLaunched {
+    launchesPast(limit: 10) {
+      id
+      mission_name
+    }
+  }
+`;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://api.spacex.land/graphql/',
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
